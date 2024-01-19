f1b1e38c246d8df0500e8667984cb42d43432176
Yifan Zhou <yifan.zhou11882-bigz@basischina.com>
2024-01-19 14:16:23 +0800

    Create Input_Layer.py

---
+++ Code/Input_Layer.py
@@ -0,0 +1,35 @@
+# import numpy and tensorflow quantum
+import numpy as np
+import tensorflow_quantum as tfq
+
+# define a function to convert a classical image to a quantum image
+def classical_to_quantum(image):
+    # assume the image is a grayscale image of size n x n
+    n = image.shape[0]
+    # normalize the pixel values to be between 0 and 1
+    image = image / 255.0
+    # create an empty quantum circuit
+    circuit = cirq.Circuit()
+    # create a list of qubits
+    qubits = [cirq.GridQubit(i, j) for i in range(n) for j in range(n)]
+    # loop over the pixels and apply rotations to the qubits
+    for i in range(n):
+        for j in range(n):
+            # get the pixel value
+            pixel = image[i, j]
+            # convert the pixel value to an angle
+            angle = pixel * np.pi
+            # apply a rotation to the corresponding qubit
+            circuit.append(cirq.ry(angle)(qubits[i * n + j]))
+    # return the quantum circuit
+    return circuit
+
+# define a function to create the input layer for quantum image
+def quantum_image_input_layer(n):
+    # create an input tensor of shape (None, n, n, 1)
+    input_tensor = tf.keras.Input(shape=(n, n, 1))
+    # create a quantum circuit layer that converts the input tensor to a quantum circuit
+    circuit_layer = tfq.layers.AddCircuit()(
+        input_tensor, prepend=classical_to_quantum)
+    # return the input layer
+    return tf.keras.Model(inputs=input_tensor, outputs=circuit_layer)
