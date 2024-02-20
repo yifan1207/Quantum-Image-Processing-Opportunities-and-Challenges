0265d11d4ff5e0e6e4d5e9dc8a582976f5ba5f01
Yifan Zhou <yifan.zhou11882-bigz@basischina.com>
2024-02-20 09:36:28 +0800

    Update Training.py

--- Code/Training.py
+++ Code/Training.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
